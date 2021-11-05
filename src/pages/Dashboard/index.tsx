7f8a3462cb4ac768551e21e738ec556b93139078
Henrique Souza <henrique.asouza@outlook.com>
2021-11-05 20:51:10 -0300

    chore: add styled components and first pages

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './styles'
+
+export default function Dashboard() {
+  return <S.h1>Dashboard</S.h1>
+}
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './styles'
+
+export default function Home() {
+  return <S.h1>Main</S.h1>
+}
