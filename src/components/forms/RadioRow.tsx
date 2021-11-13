e60119b3625f99fd1f81afcc614c945b9b12f06e
Henrique Souza <henrique.asouza@outlook.com>
2021-11-12 23:44:36 -0300

    chore: add radio row component

---
+++ src/components/forms/RadioRow.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormControl from '@mui/material/FormControl'
+import FormLabel from '@mui/material/FormLabel'
+
+type ValueProps = {
+  label: string
+  value: boolean
+}
+
+type Props = {
+  label: string
+  values: ValueProps[]
+  handler: (value: boolean) => void
+}
+
+export default function RowRadioButtonsGroup({
+  label,
+  values,
+  handler
+}: Props) {
+  const [value, setValue] = React.useState(false)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if ((event.target as HTMLInputElement).value === 'true') {
+      handler(true)
+      setValue(true)
+    } else {
+      handler(false)
+      setValue(false)
+    }
+  }
+  return (
+    <FormControl component="fieldset" fullWidth>
+      <FormLabel component="legend">{label}</FormLabel>
+      <RadioGroup
+        row
+        aria-label="gender"
+        name="row-radio-buttons-group"
+        value={value}
+        onChange={handleChange}
+      >
+        {values.map((value, index) => (
+          <FormControlLabel
+            key={index}
+            value={value.value}
+            control={<Radio />}
+            label={value.label}
+          />
+        ))}
+      </RadioGroup>
+    </FormControl>
+  )
+}
