86702720c59fad8d47d1fe68cf1d818a2f9be2e0
Henrique Souza <henrique.asouza@outlook.com>
2021-11-13 12:50:25 -0300

    chore(multilinetext): add handler

--- src/components/forms/MultilineText.tsx
+++ src/components/forms/MultilineText.tsx
@@ -5,13 +5,20 @@ type Props = {
   label: string
   id: string
   rows: number
+  handler: (input: string) => void
 }
 
-export default function MultilineTextFields({ label, id, rows }: Props) {
+export default function MultilineTextFields({
+  label,
+  id,
+  rows,
+  handler
+}: Props) {
   const [value, setValue] = React.useState('')
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value)
+    handler(event.target.value)
   }
 
   return (
