f59893ebbc83a8bba4654d8b7f4ed36f089742db
Henrique Souza <henrique.asouza@outlook.com>
2021-11-13 18:39:17 -0300

    chore(student): type with complete fields declarations

--- src/types/Student.ts
+++ src/types/Student.ts
@@ -3,11 +3,40 @@ export type StudentProp = {
   name: string
   subname: string
   responsible: string
-  phone?: string
-  email?: string
-  city?: string
-  receiveINSS?: boolean
+  phone: string
+  email: string
+  receiveINSS: boolean
+  isAPAE: boolean
   birthDate: Date
+  school: string
+  schoolSerie: string
+  // Adress
+  street: string
+  district: string
+  zipcode: string
+  city: string
+  state: string
+  residence: string
+  rentValue: number
+  // Mother Section
+  motherName: string
+  motherDN: Date
+  motherWorkplace: string
+  motherIncome: number
+  motherAgeChildBorn: number
+  motherSchooling: string
+  // Mother Section
+  fatherName: string
+  fatherDN: Date
+  fatherWorkplace: string
+  fatherIncome: number
+  fatherSchooling: string
+  // Family | Others
+  familyComposition: FamilyMemberProp[]
+  familyIncome: number
+  CEI: string
+  othersActivities?: string
+  obs?: string
 }
 
 export type FamilyMemberProp = {
