5f27aa2747e40efb76f53a55cde0e01d499d2504
Henrique Souza <henrique.asouza@outlook.com>
2021-11-14 00:11:41 -0300

    fix(studentothers): component now handles edit properly

--- src/components/forms/student/StudentOthers.tsx
+++ src/components/forms/student/StudentOthers.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import { Chip, Divider } from '@mui/material'
 import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
@@ -10,50 +11,55 @@ type Props = {
   props?: StudentProp
 }
 
-const StudentOthers = ({ handleObs, handleAct, props }: Props) => (
-  <>
-    <Divider variant="middle" sx={{ alignItems: 'center' }}>
-      <Chip label="Outros" />
-    </Divider>
-    <Grid container spacing={3} sx={{ mb: 6 }}>
-      <Grid item xs={6} spacing={3} sx={{ mt: 4 }}>
-        {/* LINK -  Others Activities */}
-        <Grid item xs={12}>
-          <MultilineText
-            id="othersActivities"
-            label="Outros"
-            rows={5}
-            handler={handleAct}
-            inputValue={props?.othersActivities}
-          />
+const StudentOthers = ({ handleObs, handleAct, props }: Props) => {
+  const [cei, setCei] = React.useState(props?.CEI && props.CEI)
+
+  return (
+    <>
+      <Divider variant="middle" sx={{ alignItems: 'center' }}>
+        <Chip label="Outros" />
+      </Divider>
+      <Grid container spacing={3} sx={{ mb: 6 }}>
+        <Grid item xs={6} spacing={3} sx={{ mt: 4 }}>
+          {/* LINK -  Others Activities */}
+          <Grid item xs={12}>
+            <MultilineText
+              id="othersActivities"
+              label="Outros"
+              rows={5}
+              handler={handleAct}
+              inputValue={props?.othersActivities}
+            />
+          </Grid>
+        </Grid>
+        <Grid item xs={6} spacing={3} sx={{ mt: 4 }}>
+          {/* LINK -  Obs */}
+          <Grid item xs={12}>
+            <MultilineText
+              id="observations"
+              label="Obs"
+              rows={2}
+              handler={handleObs}
+              inputValue={props?.obs}
+            />
+          </Grid>
+          {/* LINK -  CEI*/}
+          <Grid item xs={12} sx={{ mt: 1 }}>
+            <TextField
+              required
+              id="inputCei"
+              name="inputCei"
+              label="CEI"
+              fullWidth
+              variant="standard"
+              onChange={(input) => setCei(input.target.value as string)}
+              value={cei}
+            />
+          </Grid>
         </Grid>
       </Grid>
-      <Grid item xs={6} spacing={3} sx={{ mt: 4 }}>
-        {/* LINK -  Obs */}
-        <Grid item xs={12}>
-          <MultilineText
-            id="observations"
-            label="Obs"
-            rows={2}
-            handler={handleObs}
-            inputValue={props?.obs}
-          />
-        </Grid>
-        {/* LINK -  CEI*/}
-        <Grid item xs={12} sx={{ mt: 1 }}>
-          <TextField
-            required
-            id="inputCei"
-            name="inputCei"
-            label="CEI"
-            fullWidth
-            variant="standard"
-            value={props?.CEI}
-          />
-        </Grid>
-      </Grid>
-    </Grid>
-  </>
-)
+    </>
+  )
+}
 
 export default StudentOthers
