e4f7e0a5856a85b0319ccf4404bfddb05214b143
Henrique Souza <henrique.asouza@outlook.com>
2021-11-14 00:03:38 -0300

    fix(studentfather): component now handles edit properly

--- src/components/forms/student/StudentFather.tsx
+++ src/components/forms/student/StudentFather.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
 import { Chip, Divider } from '@mui/material'
@@ -11,90 +12,126 @@ type Props = {
   props?: StudentProp
 }
 
-const StudentFather = ({ handleIncome, handleBirth, props }: Props) => (
-  <>
-    <Divider variant="middle" sx={{ alignItems: 'center' }}>
-      <Chip label="Dados do Pai" />
-    </Divider>
-    <Grid container spacing={3} sx={{ mb: 4 }}>
-      <Grid item xs={12} sm={4}>
-        {/* LINK - Father Name */}
-        <TextField
-          required
-          id="fatherName"
-          name="fatherName"
-          label="Nome"
-          fullWidth
-          autoComplete="given-name"
-          variant="standard"
-          value={props?.fatherName}
-        />
+const StudentFather = ({ handleIncome, handleBirth, props }: Props) => {
+  const [name, setName] = React.useState(
+    props?.fatherName ? props?.fatherName : ''
+  )
+  const [subName, setSubName] = React.useState(
+    props?.fatherSubName ? props?.fatherSubName : ''
+  )
+  const [fatherDN, setFatherDN] = React.useState<Date>()
+  const [workplace, setWorkplace] = React.useState(
+    props?.fatherWorkplace ? props.fatherWorkplace : ''
+  )
+  const [income, setIncome] = React.useState(
+    props?.fatherIncome && props.fatherIncome
+  )
+
+  const [schooling, setSchooling] = React.useState(
+    props?.fatherSchooling && props.fatherSchooling
+  )
+
+  React.useEffect(() => {
+    if (props?.fatherDN) {
+      setFatherDN(props.fatherDN)
+    }
+  }, [props?.fatherDN])
+  return (
+    <>
+      <Divider variant="middle" sx={{ alignItems: 'center' }}>
+        <Chip label="Dados do Pai" />
+      </Divider>
+      <Grid container spacing={3} sx={{ mb: 4 }}>
+        <Grid item xs={12} sm={4}>
+          {/* LINK - Father Name */}
+          <TextField
+            required
+            id="fatherName"
+            name="fatherName"
+            label="Nome"
+            fullWidth
+            autoComplete="given-name"
+            variant="standard"
+            onChange={(input) => setName(input.target.value as string)}
+            value={name}
+          />
+        </Grid>
+        {/* LINK - Father Subname */}
+        <Grid item xs={12} sm={5}>
+          <TextField
+            required
+            id="fatherSubName"
+            name="fatherSubname"
+            label="Sobrenome"
+            fullWidth
+            autoComplete="family-name"
+            variant="standard"
+            onChange={(input) => setSubName(input.target.value as string)}
+            value={subName}
+          />
+        </Grid>
+        {/* LINK - Father DN */}
+        <Grid item xs={12} sm={3}>
+          <Date
+            txtLabel="Data Nasc"
+            componentName="fatherBirthDate"
+            handler={(date) => {
+              handleBirth(date)
+              setFatherDN(date)
+            }}
+            initialValue={fatherDN}
+          />
+        </Grid>
+        {/* LINK - Father Workplace */}
+        <Grid item xs={12} sm={4}>
+          <TextField
+            required
+            id="fatherWorkplace"
+            name="fatherWorkplace"
+            label="Local Trabalo"
+            fullWidth
+            autoComplete="family-name"
+            variant="standard"
+            onChange={(input) => setWorkplace(input.target.value as string)}
+            value={workplace}
+          />
+        </Grid>
+        {/* LINK - Father Income */}
+        <Grid item xs={12} sm={4}>
+          <NumericInput
+            autoComplete="income"
+            id="fatherIncome"
+            name="fatherIncome"
+            precision={2}
+            decimalChar=","
+            thousandChar="."
+            label="Renda"
+            onChange={(event) => {
+              const incomeValue = event.target.value as number
+              handleIncome(incomeValue)
+              setIncome(incomeValue)
+            }}
+            variant="standard"
+            value={income}
+          />
+        </Grid>
+        {/* LINK - Father Schooling */}
+        <Grid item xs={12} sm={4}>
+          <TextField
+            required
+            id="fatherSchooling"
+            name="fatherSchooling"
+            label="Escolaridade"
+            fullWidth
+            autoComplete="father-schooling"
+            variant="standard"
+            onChange={(event) => setSchooling(event.target.value as string)}
+            value={schooling}
+          />
+        </Grid>
       </Grid>
-      {/* LINK - Father Subname */}
-      <Grid item xs={12} sm={5}>
-        <TextField
-          required
-          id="fatherSubName"
-          name="fatherSubname"
-          label="Sobrenome"
-          fullWidth
-          autoComplete="family-name"
-          variant="standard"
-          //value={props?.fatherSubName}
-        />
-      </Grid>
-      {/* LINK - Father DN */}
-      <Grid item xs={12} sm={3}>
-        <Date
-          txtLabel="Data Nasc"
-          componentName="fatherBirthDate"
-          handler={handleBirth}
-          initialValue={props?.fatherDN}
-        />
-      </Grid>
-      {/* LINK - Father Workplace */}
-      <Grid item xs={12} sm={4}>
-        <TextField
-          required
-          id="fatherWorkplace"
-          name="fatherWorkplace"
-          label="Local Trabalo"
-          fullWidth
-          autoComplete="family-name"
-          variant="standard"
-          value={props?.fatherWorkplace}
-        />
-      </Grid>
-      {/* LINK - Father Income */}
-      <Grid item xs={12} sm={4}>
-        <NumericInput
-          autoComplete="income"
-          id="fatherIncome"
-          name="fatherIncome"
-          precision={2}
-          decimalChar=","
-          thousandChar="."
-          label="Renda"
-          onChange={(event) => handleIncome(event.target.value as number)}
-          variant="standard"
-          value={props?.fatherIncome}
-        />
-      </Grid>
-      {/* LINK - Father Schooling */}
-      <Grid item xs={12} sm={4}>
-        <TextField
-          required
-          id="fatherSchooling"
-          name="fatherSchooling"
-          label="Escolaridade"
-          fullWidth
-          autoComplete="father-schooling"
-          variant="standard"
-          value={props?.fatherSchooling}
-        />
-      </Grid>
-    </Grid>
-  </>
-)
+    </>
+  )
+}
 
 export default StudentFather
