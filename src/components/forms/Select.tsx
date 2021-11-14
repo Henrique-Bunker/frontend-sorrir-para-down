b4468108951c02ba3e27a8f95447b0aa1c7406b8
Henrique Souza <henrique.asouza@outlook.com>
2021-11-13 21:03:42 -0300

    chore(select): component support initial input value

--- src/components/forms/Select.tsx
+++ src/components/forms/Select.tsx
@@ -16,16 +16,39 @@ type Props = {
   id: string
   values: ValueProps[]
   handler: (value: string) => void
+  inputValue?: string
 }
 
 export default function SelectLabels({
-  label,
+  inputValue,
   hint,
   id,
   values,
   handler
 }: Props) {
-  const [value, setValue] = React.useState('')
+  const [value, setValue] = React.useState(inputValue ? inputValue : '')
+  const [label, setLabel] = React.useState('')
+
+  React.useEffect(() => {
+    switch (inputValue) {
+      case 'proprieatary':
+        setLabel('Propria')
+        break
+      case 'rent':
+        setLabel('Alugada')
+        break
+      case 'given':
+        setLabel('Cedida')
+        break
+      case 'others':
+        setLabel('Outros')
+        break
+
+      default:
+        setLabel('Casa')
+        break
+    }
+  }, [value, inputValue])
 
   const handleChange = (event: SelectChangeEvent) => {
     setValue(event.target.value)
