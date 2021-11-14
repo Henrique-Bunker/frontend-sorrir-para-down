c5cf4cfb8be3c63051bd687f3b072185c578ad45
Henrique Souza <henrique.asouza@outlook.com>
2021-11-14 16:28:46 -0300

    chore(routes): add report route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import Main from './pages/Main'
 import Login from './pages/Login'
 import StudentsPage from './pages/StudentsPage'
 import UsersPage from './pages/UsersPage'
+import ReportsPage from './pages/ReportsPage'
 import { BrowserRouter, Route, Routes as ReactRoutes } from 'react-router-dom'
 
 const Routes = () => {
@@ -14,6 +15,7 @@ const Routes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/alunos" element={<StudentsPage gridSize={12} />} />
         <Route path="/usuarios" element={<UsersPage gridSize={6} />} />
+        <Route path="/relatorios" element={<ReportsPage gridSize={12} />} />
       </ReactRoutes>
     </BrowserRouter>
   )
